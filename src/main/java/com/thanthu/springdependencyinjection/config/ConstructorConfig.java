3b8edea27f0b8a9ce83258894b0b04b3e1f47428
Thanthu <tnair@orthofx.com>
2021-08-05 17:48:12 +0530

    remove unused imports

--- src/main/java/com/thanthu/springdependencyinjection/config/ConstructorConfig.java
+++ src/main/java/com/thanthu/springdependencyinjection/config/ConstructorConfig.java
@@ -2,8 +2,6 @@ package com.thanthu.springdependencyinjection.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
 @ConstructorBinding
 @ConfigurationProperties("datasource")
